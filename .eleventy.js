6ddc5bee98f2b6afcaf856026207f237d8984b72
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-12-03 09:24:44 +0100

    Run prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,12 @@
-module.exports = function(eleventyConfig) {
-    return {
-        dir: {
-            input: "src",
-            output: "_site",
-            data: "_data",
-        },
-        templateFormats: ["html", "njk", "md", "11ty.js"],
-        markdownTemplateEngine: "njk",
-        htmlTemplateEngine: "njk",
-    };
-}
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      data: "_data",
+    },
+    templateFormats: ["html", "njk", "md", "11ty.js"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+  };
+};
